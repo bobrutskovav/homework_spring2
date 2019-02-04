1d2ae63b302d63f2f5688b0c0baa462c8d120dcd
bobrutskovav <thirdstrike@yandex.ru>
2019-02-04 22:15:15 +0300

    Добавлен монгок (замена mongobee)

--- src/main/java/ru/otus/dao/BookRepository.java
+++ src/main/java/ru/otus/dao/BookRepository.java
@@ -2,11 +2,12 @@ package ru.otus.dao;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.otus.domain.Book;
 
 import java.util.List;
 
-
+@Repository
 public interface BookRepository extends MongoRepository<Book, String> {
 
     Book findByTitle(String title);
--- src/main/java/ru/otus/dao/CommentRepository.java
+++ src/main/java/ru/otus/dao/CommentRepository.java
@@ -1,7 +1,9 @@
 package ru.otus.dao;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import ru.otus.domain.Comment;
 
+@Repository
 public interface CommentRepository extends MongoRepository<Comment, String> {
 }
