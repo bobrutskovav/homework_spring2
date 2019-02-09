05f2856020bbc13a449f1b64af67577492ffee29
bobrutskovav <thirdstrike@yandex.ru>
2019-02-09 23:17:54 +0300

    Сделал кнопку Добавить Новую Книгу и условие для вывода списка книг

--- src/main/java/ru/otus/controller/BookController.java
+++ src/main/java/ru/otus/controller/BookController.java
@@ -5,23 +5,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.otus.dao.BookRepository;
 import ru.otus.service.BookService;
 
 @Controller
 public class BookController {
 
 
-    BookService bookService;
+    private BookService bookService;
+    private BookRepository bookRepository;
 
 
     @Autowired
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, BookRepository bookRepository) {
         this.bookService = bookService;
+        this.bookRepository = bookRepository;
     }
 
     @GetMapping("/library")
     public String libraryPage(Model model) {
-        bookService.printAllBooks();
+        model.addAttribute("allBooks", bookRepository.findAll());
         return "library";
     }
 }
