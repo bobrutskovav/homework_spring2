20b21c4dd23e0d4a6f3e0ab1d358b408d99c57d3
bobrutskovav <thirdstrike@yandex.ru>
2019-02-10 22:31:10 +0300

    Добавил редактирование книги

--- src/main/java/ru/otus/controller/BookController.java
+++ src/main/java/ru/otus/controller/BookController.java
@@ -5,7 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.otus.dao.BookRepository;
+import ru.otus.domain.Book;
+import ru.otus.domain.BookNotFoundException;
 import ru.otus.service.BookService;
 
 @Controller
@@ -30,6 +35,29 @@ public class BookController {
 
     @GetMapping("/library/newbook")
     public String newBookPage(Model model) {
-        return "newBook";
+        model.addAttribute("book", new Book());
+        return "newbook";
+    }
+
+    @PostMapping("/library/newbook")
+    public String addBook(@ModelAttribute Book book, Model model) {
+        bookService.storeNewBook(book.getTitle(), book.getAuthor().getName(), book.getGenre().getTitle());
+        model.addAttribute("allBooks", bookRepository.findAll());
+        return "library";
+    }
+
+    @GetMapping("/library/edit")
+    public String editPage(@RequestParam("id") String id, Model model) throws BookNotFoundException {
+        Book bookForEdit = bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException());
+        model.addAttribute("bookForEdit", bookForEdit);
+        return "edit";
+    }
+
+    @PostMapping("/library/edit")
+    public String updateBook(@ModelAttribute Book book, Model model) {
+        bookRepository.save(book);
+        model.addAttribute("allBooks", bookRepository.findAll());
+        return "library";
+
     }
 }
