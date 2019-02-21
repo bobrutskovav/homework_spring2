311e66ecd809870621942b874a44a039aedc6dc8
bobrutskovav <thirdstrike@yandex.ru>
2019-02-21 23:18:57 +0300

    js начал запускаться

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,14 +1,11 @@
-function getAllBooks(){
-    var data;
-    fetch('/library').then(response => data = response.json());
-    return data;
-}
-
-
-var app = new Vue({
-el: '#vue-app',
-data: {
-    allBooks : getAllBooks()
-}
+fetch('/library').then(response = > response.json()
+).
+then(books = > new Vue({
+    el: '#vue-app',
+    data: {
+        allBooks: books
+    }
 })
-console.log(allBooks)
+)
+;
+
