f1ab55e695dcf4094470d4968a40af52306b8a07
bobrutskovav <thirdstrike@yandex.ru>
2019-02-27 22:47:26 +0300

    Добавил добавление новой книги

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -3,7 +3,13 @@ const app = new Vue({
     data: {
         displayedBooks: [],
         currentBookToSearch: '',
-        newBookAreaDisplayed: false;
+        newBookAreaDisplayed: false,
+        newBook: {
+            title: '',
+            author: '',
+            genre: '',
+            someInputIsEmpty: true
+        }
     },
     created() {
         let self = this;
@@ -46,6 +52,37 @@ const app = new Vue({
                     .then(response => response.json())
                     .then((data) => app.displayedBooks = data)
             });
+
+        },
+        addNewBook: function () {
+            // if (this.newBook.someInputIsEmpty) {
+            //     alert("Неоходимо заполнить все поля в информации о новой книге");
+            //
+            // }
+            fetch("library/book", {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    title: this.newBook.title,
+                    author: this.newBook.author,
+                    genre: this.newBook.genre
+                })
+            }).then((response) => {
+                if (response.status != '201') {
+                    throw new Error('PostBook response was not ok for book with id ' + bookId);
+                }
+            }).finally(() => {
+                this.newBook.title = '';
+                this.newBook.author = '';
+                this.newBook.genre = '';
+                this.newBookAreaDisplayed = false;
+                getAllBooks()
+                    .then(response => response.json())
+                    .then((data) => app.displayedBooks = data);
+            })
 
         }
     }
