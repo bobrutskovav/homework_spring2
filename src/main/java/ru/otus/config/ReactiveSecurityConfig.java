82095518a722d51d0c58d6648e11973d445066de
bobrutskovav <thirdstrike@yandex.ru>
2019-04-05 22:08:42 +0300

    Security. Origins. Создал чейн. Пока не понятно.

--- src/main/java/ru/otus/config/ReactiveSecurityConfig.java
+++ src/main/java/ru/otus/config/ReactiveSecurityConfig.java
@@ -2,8 +2,14 @@ package ru.otus.config;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @EnableWebFluxSecurity
@@ -12,8 +18,23 @@ public class ReactiveSecurityConfig {
     @Bean
     public SecurityWebFilterChain securitygWebFilterChain(
             ServerHttpSecurity http) {
-        return http.authorizeExchange().pathMatchers("/edit").hasAuthority("ROLE_ADMIN")
+        return http.authorizeExchange()
+                .pathMatchers(HttpMethod.DELETE, "/library/book/**").hasAuthority("ROLE_ADMIN")
                 .and().authorizeExchange().anyExchange().permitAll()
-                .and().formLogin().and().build();
+                .and().formLogin().and()
+                .csrf().disable().build();
+    }
+
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        UserDetails user = User
+                .withUsername("LibraryMan")
+                .password(passwordEncoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+        return new MapReactiveUserDetailsService(user);
     }
 }
